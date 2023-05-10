9c5a24a6122cb2aafdfd95f0f76b24ef0cfe1715
Dina Migunova <migunova.dina@gmail.com>
2023-05-10 14:42:34 +0200

    update

---
+++ HW/9/index.js
@@ -0,0 +1,16 @@
+let x = ['Семен', 'Иван', 'Петр','Татьяна'];
+let y = [18, 27, 74, 34];
+
+let z = new Array;
+for(let i = 0; i < x.length; i++) {
+    z.push(x[i] + ' ' + y[i] + 'годов/лет');
+}
+
+let a = [1, 2, 3, 4];
+let b = new Array;
+
+for (let i = 0; i < a.length; i++) {
+    b.unshift(a[i]);
+}
+
+
